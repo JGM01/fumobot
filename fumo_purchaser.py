1f963a69de70607b4b4698327994bf85bb194a5c
JGM01 <jacobgermanamccray@gmail.com>
2022-04-27 18:04:08 -0400

    Optimized

--- fumo_purchaser.py
+++ fumo_purchaser.py
@@ -1,12 +1,19 @@
+from lib2to3.pgen2 import driver
+import threading
+from typing import final
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-
+from selenium.webdriver.firefox.options import Options
+from time import sleep
+from datetime import datetime
 
 # CSS Selectors 
+CREDIT_CARD_LABEL = '//label[contains(text(), "Credit card")]'
+SHIPPING_METHOD_LABEL = "label[class='form-radio-label'][for='shipping-method19'] span"
 WEBSITE = 'https://secure.amiami.com/eng/'
 PRODUCT_DETAIL_ADD_CART_8 = '[data-kanshi="productDetailAddCart8"]'
 CART_SUBMIT = '[data-kanshi="cartSubmit"]'
@@ -14,20 +21,35 @@ CART_MATOME_1_SUBMIT = '[data-kanshi="cartMatome1Submit"]'
 CART_MATOME_2_SUBMIT = '[data-kanshi="cartMatome2Submit"]'
 CART_MATOME_3_SUBMIT = '[data-kanshi="cartMatome3Submit"]'
 
+ADD_TO_CART_PAGE = 'https://www.amiami.com/eng/cart/'
 
-class FumoPurchaser:
+class FumoPurchaser():
     def __init__(self, fumo, username, password):
-        self.driver = webdriver.Firefox()
+        #threading.Thread.__init__(self)        
+        driver_options = Options()
+        driver_options.add_argument('--headless')
+        self.driver = webdriver.Firefox(options=driver_options)
+        print('[FUMO PURCHASER] Initialized ', end='')
+        print(self.driver)
         self.fumo = fumo
         self.username = username
         self.password = password
 
-    def execute(self):
-        self.driver.get(WEBSITE)
-        self.login()
-        self.purchase_fumo()
-        self.confirm_purchase()
-    
+    def run(self):
+        try:
+            start = datetime.now()
+            self.purchase_fumo()
+            print('added to cart')
+            self.confirm_purchase()
+            print('confirmed purchase')
+        except Exception as e:
+            print(e)
+        finally:
+            self.driver.quit()
+            print(self.driver, end='')
+            print(datetime.now()-start)            
+
+
     def login(self):
         WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.NAME, 'email'))).send_keys(self.username)
         WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.NAME, 'password'))).send_keys(self.password)
@@ -42,16 +64,17 @@ class FumoPurchaser:
     
     def confirm_purchase(self):
         WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, CART_MATOME_1_SUBMIT))).click()
-        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//label[contains(text(), "Credit card")]'))).click()
-        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "label[class='form-radio-label'][for='shipping-method19'] span"))).click()
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, CREDIT_CARD_LABEL))).click()
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, SHIPPING_METHOD_LABEL))).click()
         WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, CART_MATOME_2_SUBMIT))).click()
-        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, CART_MATOME_3_SUBMIT))).click()
+        #WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, CART_MATOME_3_SUBMIT))).click()
     
     def idle(self):
         while True:
             try:
-                WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, PRODUCT_DETAIL_ADD_CART_8)))
+                WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, PRODUCT_DETAIL_ADD_CART_8)))
                 break
             except TimeoutException:
                 self.driver.refresh()
+                print('refreshed')
                 continue
--- main.py
+++ main.py
@@ -1,31 +1,25 @@
 from fumo_purchaser import FumoPurchaser
-from concurrent.futures import ThreadPoolExecutor
 import sys
-from datetime import datetime
+from concurrent import futures
 
-start = datetime.now()
-
-# Test URLs, not the actual fumos
 Fumos = [
-    'https://www.amiami.com/eng/detail/?gcode=GOODS-04228618', 
-    'https://www.amiami.com/eng/detail/?gcode=GOODS-04228617',
-    'https://www.amiami.com/eng/detail/?gcode=GOODS-04228616',
-    'https://www.amiami.com/eng/detail/?gcode=GOODS-04228619']
+    'https://www.amiami.com/eng/detail/?gcode=FIGURE-138984', 
+    'https://www.amiami.com/eng/detail/?gcode=FIGURE-138817',
+    'https://www.amiami.com/eng/detail?gcode=GOODS-04233282',
+    'https://www.amiami.com/eng/detail?gcode=GOODS-04233294',
+    'https://www.amiami.com/eng/detail/?gcode=GOODS-04229578']
 
 USERNAME = sys.argv[1]
 PASSWORD = sys.argv[2]
-MAX_THREADS = int(sys.argv[3])
 
 def spawn_FumoPurchaser(fumo):
     bot = FumoPurchaser(fumo, USERNAME, PASSWORD)
-    bot.execute()    
-    print("Success!")
+    bot.run()  
 
 def main():
-    with ThreadPoolExecutor(max_workers = MAX_THREADS) as executor:
-        results = executor.map(spawn_FumoPurchaser, Fumos)
+    with futures.ThreadPoolExecutor() as executor:
+        purchasers = list(executor.map(spawn_FumoPurchaser, Fumos))
 
 if __name__ == "__main__":
     main()
     print(sys.argv)
-    print(datetime.now()-start)
